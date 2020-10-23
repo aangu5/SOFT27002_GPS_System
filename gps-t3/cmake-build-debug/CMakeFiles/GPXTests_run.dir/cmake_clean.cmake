379237584ebb269f05ed00f507a34ba9faacbae9
N0925090 <angus.cheney2019@my.ntu.ac.uk>
2020-10-23 22:02:33 +0100

    Added CMakeLists.txt and build files

---
+++ gps-t3/cmake-build-debug/CMakeFiles/GPXTests_run.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GPXTests_run.dir/mnt/c/Users/Angus/CLionProjects/gps1/gps-analysing_routes/src/route.cpp.o"
+  "CMakeFiles/GPXTests_run.dir/mnt/c/Users/Angus/CLionProjects/gps1/gps-analysing_routes/src/xml/element.cpp.o"
+  "CMakeFiles/GPXTests_run.dir/mnt/c/Users/Angus/CLionProjects/gps1/gps-analysing_routes/src/xml/parser.cpp.o"
+  "CMakeFiles/GPXTests_run.dir/mnt/c/Users/Angus/CLionProjects/gps1/gps-t2/src/earth.cpp.o"
+  "CMakeFiles/GPXTests_run.dir/mnt/c/Users/Angus/CLionProjects/gps1/gps-t2/src/geometry.cpp.o"
+  "CMakeFiles/GPXTests_run.dir/mnt/c/Users/Angus/CLionProjects/gps1/gps-t2/src/logs.cpp.o"
+  "CMakeFiles/GPXTests_run.dir/mnt/c/Users/Angus/CLionProjects/gps1/gps-t2/src/position.cpp.o"
+  "CMakeFiles/GPXTests_run.dir/src/gpx-tests.cpp.o"
+  "GPXTests_run"
+  "GPXTests_run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GPXTests_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
