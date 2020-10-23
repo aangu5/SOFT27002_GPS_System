c18e4c51f3c6f66de0206b34ba99e158e249e45a
N0925090 <angus.cheney2019@my.ntu.ac.uk>
2020-10-23 22:53:00 +0100

    Added bad_input_string to test cases

--- gps-t3/src/gpx-tests/timesVisited(string).cpp
+++ gps-t3/src/gpx-tests/timesVisited(string).cpp
@@ -17,7 +17,7 @@ const bool isFileName = false;
     BOOST_AUTO_TEST_CASE( singleton_route )
     {
         const std::string gpxData =
-                "<gpx><rte><name>MyRoute</name><rtept lat=\"0\" lon=\"0\"><name>MyPosition</name></rtept></rte></gpx>";
+                R"(<gpx><rte><name>MyRoute</name><rtept lat="0" lon="0"><name>MyPosition</name></rtept></rte></gpx>)";
         Route route = Route(gpxData, isFileName);
         BOOST_CHECK_EQUAL( route.timesVisited("MyPosition"), 1 );
     }
@@ -26,7 +26,7 @@ const bool isFileName = false;
     BOOST_AUTO_TEST_CASE( singleton_route_with_spaces )
     {
         const std::string gpxData =
-                "<gpx><rte><name>MyRoute</name><rtept lat=\"0\" lon=\"0\"><name>    My Position    </name></rtept></rte></gpx>";
+                R"(<gpx><rte><name>MyRoute</name><rtept lat="0" lon="0"><name>    My Position    </name></rtept></rte></gpx>)";
         Route route = Route(gpxData, isFileName);
         BOOST_CHECK_EQUAL( route.timesVisited("My Position"), 1 );
     }
@@ -35,9 +35,18 @@ const bool isFileName = false;
     BOOST_AUTO_TEST_CASE( position_not_visited )
     {
         const std::string gpxData =
-                "<gpx><rte><name>MyRoute</name><rtept lat=\"0\" lon=\"0\"></rtept></rte></gpx>";
+                R"(<gpx><rte><name>MyRoute</name><rtept lat="0" lon="0"></rtept></rte></gpx>)";
         Route route = Route(gpxData, isFileName);
         BOOST_CHECK_EQUAL( route.timesVisited("Your Position"), 0 );
+    }
+
+    // A simple route with one point and checking invalid_argument is thrown when a blank string is passed in.
+    BOOST_AUTO_TEST_CASE( bad_input_string )
+    {
+        const std::string gpxData =
+                R"(<gpx><rte><name>MyRoute</name><rtept lat="0" lon="0"><name>MyPosition</name></rtept></rte></gpx>)";
+        Route route = Route(gpxData, isFileName);
+        BOOST_CHECK_THROW( route.timesVisited(""), std::invalid_argument );
     }
 
 BOOST_AUTO_TEST_SUITE_END()
