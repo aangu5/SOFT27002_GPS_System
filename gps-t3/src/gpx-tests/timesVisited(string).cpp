e1f7b8d6b2aa8b0172be2f3698cf43d2b7ffb403
N0925090 <angus.cheney2019@my.ntu.ac.uk>
2020-10-23 21:34:47 +0100

    Initial Test Suite setup

---
+++ gps-t3/src/gpx-tests/timesVisited(string).cpp
@@ -0,0 +1,14 @@
+//
+// Created by Angus on 23/10/2020.
+//
+#include <boost/test/unit_test.hpp>
+
+#include "types.h"
+#include "route.h"
+
+using namespace GPS;
+
+BOOST_AUTO_TEST_SUITE( Route_timesVisited(string) )
+
+
+BOOST_AUTO_TEST_SUITE_END()
