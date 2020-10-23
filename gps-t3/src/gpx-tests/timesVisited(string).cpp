1dbdc6fcd97850bcf3a549c7ce7321175340406a
N0925090 <angus.cheney2019@my.ntu.ac.uk>
2020-10-23 22:42:58 +0100

    Added second test case to timesVisited(string).cpp

--- gps-t3/src/gpx-tests/timesVisited(string).cpp
+++ gps-t3/src/gpx-tests/timesVisited(string).cpp
@@ -22,4 +22,13 @@ const bool isFileName = false;
         BOOST_CHECK_EQUAL( route.timesVisited("MyPosition"), 1 );
     }
 
+    // A simple route with one point and one name to check, with leading and trailing spaces to check the method matches the constructor.
+    BOOST_AUTO_TEST_CASE( singleton_route_with_spaces )
+    {
+        const std::string gpxData =
+                "<gpx><rte><name>MyRoute</name><rtept lat=\"0\" lon=\"0\"><name>    My Position    </name></rtept></rte></gpx>";
+        Route route = Route(gpxData, isFileName);
+        BOOST_CHECK_EQUAL( route.timesVisited("My Position"), 1 );
+    }
+
 BOOST_AUTO_TEST_SUITE_END()
